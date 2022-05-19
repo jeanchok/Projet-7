f6aced1e98cc66d97330fa26888ff39e3f83f4c1
jeanchok <48122647+jeanchok@users.noreply.github.com>
2022-05-19 15:18:34 +0200

    Update comment.js

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -11,5 +11,4 @@ router.get('/:id', auth, commentsCtrl.getOneComment);
 router.put('/:id', auth, multer, commentsCtrl.modifyComment);
 router.delete('/:id', auth, commentsCtrl.deleteComment);
 
-
 module.exports = router;
